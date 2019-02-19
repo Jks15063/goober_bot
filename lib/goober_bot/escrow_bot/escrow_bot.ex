a5bb16cf5e815a6d4676894c8e72c6e47a5c8d63
jks15063 <jks15063@gmail.com>
2019-02-19 11:56:27 -0700

    rename file

---
+++ lib/goober_bot/escrow_bot/escrow_bot.ex
@@ -0,0 +1,100 @@
+defmodule GooberBot.EscrowBot do
+  @moduledoc """
+  Bot designed to facilitate money matches on discord
+  """
+
+  use GenServer
+  use Nostrum.Consumer
+
+  alias Nostrum.Api
+
+  def start_link do
+    IO.inspect("HI")
+    Consumer.start_link(__MODULE__)
+  end
+
+  # is this ever run???
+  def init(_initial_state) do
+    IO.inspect("HELLO")
+    :ets.new(:challenge_table, [:named_table, :private])
+    :ets.insert(:challenge_table, {:foo, true})
+    :ets.last(:challenge_table) |> IO.inspect()
+    {:ok, :nostate}
+  end
+
+  def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
+    case msg.content do
+      "$eb challenge" <> challenge_txt ->
+        [_full_match, challenged_user_id, dollar_amount] =
+          Regex.run(~r/<@(\w+)> \$([0-9]+)/, challenge_txt)
+
+        Api.create_message(
+          msg.channel_id,
+          "<@#{challenged_user_id}> has been challenged by <@#{msg.author.id}> for $#{
+            dollar_amount
+          }."
+        )
+
+      "$eb accept" ->
+        :ets.insert(:challenge_table, {:foo, true})
+
+        :ets.last(:challenge_table) |> IO.inspect()
+        # :ets.first(:challenge_table) |> IO.inspect()
+
+        # IO.inspect(:ets.lookup(challenge_table, :number))
+        # IO.inspect(:ets.lookup_element(challenge_table, :number, 2))
+
+        Api.create_message(
+          msg.channel_id,
+          "User <@#{msg.author.id}> has accepted a challenge from TODO"
+        )
+
+      "$eb decline" ->
+        # :ets.insert(challenge_table, {:bar, true})
+
+        # :ets.last(challenge_table) |> IO.inspect()
+        # :ets.first(challenge_table) |> IO.inspect()
+
+        Api.create_message(
+          msg.channel_id,
+          "<@#{msg.author.id}> has declined a challenge from TODO"
+        )
+
+      "$eb cancel" ->
+        Api.create_message(
+          msg.channel_id,
+          "<@#{msg.author.id}> has canceled their challenge to TODO"
+        )
+
+      "$eb report winner" <> winner ->
+        Api.create_message(
+          msg.channel_id,
+          "#{winner} has defeated TODO and won $TODO"
+        )
+
+      "$eb help" ->
+        Api.create_message(
+          msg.channel_id,
+          """
+          Issue challenge: `$eb challenge @<username> $<dollar_amount>`.
+          Example: `$eb challenge @torkable $10.00`
+
+          Accept challenge: `$accept`
+
+          Decline challenge: `$eb decline`
+
+          Cancel challenge: `$eb cancel`
+
+          Finish match: `$eb report winner @<username>`
+          """
+        )
+
+      _ ->
+        :ignore
+    end
+  end
+
+  def handle_event(_event) do
+    :noop
+  end
+end
