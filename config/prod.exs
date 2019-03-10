65bbb7bc6709ac15fe833ca44bc836fda40fa2e2
jks15063 <jks15063@gmail.com>
2019-03-09 20:27:59 -0700

    save

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,6 @@ config :goober_bot, GooberBotWeb.Endpoint,
   load_from_system_env: true,
   http: [port: {:system, "PORT"}],
   url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}"
 
@@ -75,6 +74,3 @@ config :logger, level: :info
 #
 # Note you can't rely on `System.get_env/1` when using releases.
 # See the releases documentation accordingly.
-
-# Finally import the config/prod.secret.exs which should be versioned
-# separately.
